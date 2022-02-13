335f852a74e3c7e289a077e10ef3dbde75361a9c
AAEmara <abdelrahmanemara18@gmail.com>
2022-02-13 17:23:55 +0200

    Adding try it yourself part in Ch.5

---
+++ Chapter5/try_it_yourself/more_conditional_tests.py
@@ -0,0 +1,44 @@
+# Assigning cars into lists
+cars = ['audi', 'bmw', 'subaru', 'toyota', 'ford']
+car = "audi"
+
+# Checking for equality using IF statement.
+
+print(f"Is car == {cars[0]}? I predict True.")
+print(car == cars[0])
+
+
+print(f"\nIs car == {cars[0]}? I predict False.")
+print(car == cars[0])
+
+
+print(f"\nIs car == {cars[1]}? I predict True.")
+print(car == cars[1])
+
+
+print(f"\nIs car == {cars[1]}? I predict False.")
+print(car == cars[1])
+
+
+print(f"\nIs car == {cars[2]}? I predict True.")
+print(car == cars[2])
+
+
+print(f"\nIs car == {cars[2]}? I predict False.")
+print(car == cars[2])
+
+
+print(f"\nIs car == {cars[3]}? I predict True.")
+print(car == cars[3])
+
+
+print(f"\nIs car == {cars[3]}? I predict False.")
+print(car == cars[3])
+
+
+print(f"\nIs car == {cars[4]}? I predict True.")
+print(car == cars[4])
+
+
+print(f"\nIs car == {cars[4]}? I predict False.")
+print(car == cars[4])
