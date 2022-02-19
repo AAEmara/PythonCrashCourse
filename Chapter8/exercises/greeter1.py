b920c5bc152fe800b6882c8c584bf44e04caa7bb
AAEmara <abdelrahmanemara18@gmail.com>
2022-02-19 23:59:44 +0200

    Adding files to exercises section in Ch.8

---
+++ Chapter8/exercises/greeter1.py
@@ -0,0 +1,6 @@
+# Defining a greeting function responsible for greeting the user!
+def greet_user():
+    """Display a simple greeting"""
+    print("Hello!")
+
+greet_user()
---
+++ Chapter8/exercises/greeter2.py
@@ -0,0 +1,7 @@
+# Defining a greeting function responsible for greeting the user!
+# The user must enter his username inside the parentheses.
+def greet_user(username):
+    """Display a simple greeting"""
+    print(f"Hello, {username.title()}!")
+
+greet_user('emara')
